3ee1abdda6272e5909800682394a6fec05e47b52
Gabriel Cerri <gabrielcerrih@gmail.com>
2020-12-13 16:42:02 -0300

    clone repo

--- src/components/Answers.js
+++ src/components/Answers.js
@@ -1,37 +1,39 @@
-import React, {} from 'react';
-import { Flex, Text } from '@chakra-ui/react';
+import React, { useState, useEffect } from 'react';
+import classNames from 'classnames'
+import { Flex, Spacer, Box, Text, Button, ButtonGroup } from '@chakra-ui/react';
 
 
-const Buttons = ({correctAnswer, incorrectAnswers1, incorrectAnswers2, incorrectAnswers3}) => 
-  (
+const Buttons = ({ correctAnswer, incorrectAnswers1, incorrectAnswers2, incorrectAnswers3 }) =>
+(
 
-        <Flex
-              
-                
-              as='button' 
-              h='35px' 
-              mt={3}
-              direction='row' 
-              align='center' 
-              justify='center'  
-              bg='gray.300' 
-                transition="0.5s" 
-                borderRadius='10px'
-                _hover ={{ transform: 'scale(1.1)', 
-                       bg: 'orange.400' }}
-                _focus ={{ outline : '0' }}
-                _active={{ transform: 'translateY(4px)'  }} >
+  <Flex
 
-              
+    as='button'
+    h='35px'
+    mt={3}
+    direction='row'
+    align='center'
+    justify='center'
+    bg='gray.300'
+    transition="0.5s"
+    borderRadius='10px'
+    _hover={{
+      transform: 'scale(1.1)',
+      bg: 'orange.400'
+    }}
+    _focus={{ outline: '0' }}
+    _active={{ transform: 'translateY(4px)' }} >
 
-          <Text>{correctAnswer}</Text>
-          <Text>{incorrectAnswers1}</Text>
-          <Text>{incorrectAnswers2}</Text>
-          <Text>{incorrectAnswers3}</Text>
 
-          
 
-        </Flex>
+    <Text>{correctAnswer}</Text>
+    <Text>{incorrectAnswers1}</Text>
+    <Text>{incorrectAnswers2}</Text>
+    <Text>{incorrectAnswers3}</Text>
+
+
+
+  </Flex>
 
 
 
--- src/components/Cuestionario.js
+++ src/components/Cuestionario.js
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from 'react';
-import classNames from 'classnames' 
+import React, { useState, useEffect } from 'react';
+import classNames from 'classnames'
 import { Flex, Spacer, Box, Text, Button, ButtonGroup } from '@chakra-ui/react';
 import Answers from './Answers.js'
 
@@ -13,20 +13,20 @@ const Cuestionario = () => {
 	const [incorrect_answers, setIncorrect_answers] = useState([])
 
 
-	  useEffect(() => {
+	useEffect(() => {
 
-	    fetch(API_URL)
-	      .then(res => res.json())
-	      .then(data => {
+		fetch(API_URL)
+			.then(res => res.json())
+			.then(data => {
 
-	      	console.log(data.results)
-	        setQuestions(data.results);
+				console.log(data.results)
+				setQuestions(data.results);
 
 
-	      });
+			});
 
-	  }, []);
-   
+	}, []);
+
 
 
 
@@ -34,88 +34,89 @@ const Cuestionario = () => {
 
 	// const mixAns = [correct_answer, ...incorrect_answers].sort(() => Math.random() - 0.5);
 
-   const handleAnswer = (answer) => {
+	const handleAnswer = (answer) => {
 
-      // esto es un comentario
+		// esto es un comentario
 
-   }
-
-
-	  return(
-
-		<Flex 
-
-				bg='white' 
-				w='430px' 
-				h='500px' 
-				justify='center' 
-				align='center' 
-				direction='column' 
-				borderRadius='24px'>
-
-					<Box position='absolute' top='145px'>
-
-					            <Text>
+	}
 
 
-					            	Question N°: 1
+	return (
+
+		<Flex
+			w='430px'
+			h='500px'
+			justify='center'
+			align='center'
+			direction='column'
+			borderRadius='24px'>
+
+			<Box position='absolute' top='145px'>
+
+				<Text color='white' mb='10px' align='center'> GABY Y EMA  </Text>
+
+				<Text color='white'>
 
 
-
-					            </Text>
-
-					</Box>
-
-					<Box>
-
-					 	<Text
-
-					 		dangerouslySetInnerHTML={{ __html: questions[0].question}}  
-					 		align='center' 
-					 		position = 'relative' 
-					 		top='-10px'
-					 		mx='10px'> 
+					PREGUNTAS N°: 1
 
 
 
-					  	</Text>
+				</Text>
 
-					</Box>
+			</Box>
 
-			<Flex 
-				w='400px' 
-				direction='column' 
-				mt={5} 
-				position='relative' 
-				top='60px' 
+			<Box>
+
+				<Text
+
+					//dangerouslySetInnerHTML={{ __html: questions[0].question }}
+					align='center'
+					position='relative'
+					top='-10px'
+					mx='10px'>
+
+
+
+				</Text>
+
+			</Box>
+
+			<Flex
+				w='400px'
+				direction='column'
+				mt={5}
+				position='relative'
+				top='60px'
 				w='380px' >
 
-				
 
 
-					 {questions.length > 0 ?
-					  <Flex direction='column' > 
-						<Answers style={{width:'300px'}} correctAnswer={questions[0].correct_answer} /> 
-						<Answers style={{width:'300px'}} incorrectAnswers1={questions[0].incorrect_answers[0]} /> 
-						<Answers style={{width:'300px'}} incorrectAnswers2={questions[0].incorrect_answers[1]} /> 
-						<Answers style={{width:'300px'}} incorrectAnswers3={questions[0].incorrect_answers[2]} /> 
-					 </Flex>
 
-				  		: (
+				{questions.length > 0 ?
+					<Flex direction='column' >
+						<Answers style={{ width: '300px' }} correctAnswer={questions[0].correct_answer} />
+						<Answers style={{ width: '300px' }} incorrectAnswers1={questions[0].incorrect_answers[0]} />
+						<Answers style={{ width: '300px' }} incorrectAnswers2={questions[0].incorrect_answers[1]} />
+						<Answers style={{ width: '300px' }} incorrectAnswers3={questions[0].incorrect_answers[2]} />
+					</Flex>
 
-								
+					: (
+
+
 						<Text>Cargando</Text>
-						)
+					)
 
-					 }
+				}
 
 			</Flex>
 
-		</Flex>  
+		</Flex >
 
 
-			)}
+	)
+}
 
 
- export default Cuestionario
+export default Cuestionario
 
